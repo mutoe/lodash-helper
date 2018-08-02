471e70996a964b8a9e93b67d10cdd2926f19b842
mutoe <mutoe@foxmail.com>
2018-08-02 15:24:02 +0000

    chore(core): fill methods selector core

--- src/core/types.js
+++ src/core/types.js
@@ -1,7 +1,56 @@
 export default [
-  { name: 'array' },
-  { name: 'object' },
-  { name: 'function' },
-  { name: 'number' },
-  { name: 'string' },
+  {
+    name: 'an array',
+    child: [
+      {
+        name: 'remove some elements',
+        child: [],
+      },
+      {
+        name: 'find elements in array',
+        child: [],
+      },
+      {
+        name: 'change this array',
+        child: [],
+      },
+    ],
+  },
+  {
+    name: 'some arrays',
+    child: [
+      {
+        name: 'get their intersection',
+        methods: [],
+      },
+      {
+        name: 'get their union',
+        methods: [],
+      },
+      {
+        name: 'get their complements',
+        methods: [],
+      },
+      {
+        name: 'get their XOR collection',
+        methods: [],
+      },
+    ],
+  },
+  {
+    name: 'object',
+    methods: [],
+  },
+  {
+    name: 'function',
+    methods: [],
+  },
+  {
+    name: 'number',
+    methods: [],
+  },
+  {
+    name: 'string',
+    methods: [],
+  },
 ]
