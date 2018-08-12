2aa7d42a78cdb3c0d311f32e0b0fb49ee7ec55e2
mutoe <mutoe@foxmail.com>
2018-08-12 11:48:20 +0800

    chore(core): fill string methods

--- src/core/methods/index.js
+++ src/core/methods/index.js
@@ -1,7 +1,9 @@
 import array from './array'
 import object from './object'
+import string from './string'
 
 export default {
   ...array,
   ...object,
+  ...string,
 }
---
+++ src/core/methods/string.js
@@ -0,0 +1,183 @@
+export default {
+
+  camelCase: {
+    type: 'string',
+    since: '3.0.0',
+    source: `_.camelCase([string=''])`,
+  },
+
+  capitalize: {
+    type: 'string',
+    since: '3.0.0',
+    source: `_.capitalize([string=''])`,
+  },
+
+  deburr: {
+    type: 'string',
+    since: '3.0.0',
+    source: `_.deburr([string=''])`,
+  },
+
+  endsWith: {
+    type: 'string',
+    since: '3.0.0',
+    source: `_.endsWith([string=''], [target], [position=string.length])`,
+  },
+
+  escape: {
+    type: 'string',
+    since: '0.1.0',
+    source: `_.escape([string=''])`,
+  },
+
+  escapeRegExp: {
+    type: 'string',
+    since: '3.0.0',
+    source: `_.escapeRegExp([string=''])`,
+  },
+
+  kebabCase: {
+    type: 'string',
+    since: '3.0.0',
+    source: `_.kebabCase([string=''])`,
+  },
+
+  lowerCase: {
+    type: 'string',
+    since: '4.0.0',
+    source: `_.lowerCase([string=''])`,
+  },
+
+  lowerFirst: {
+    type: 'string',
+    since: '4.0.0',
+    source: `_.lowerFirst([string=''])`,
+  },
+
+  pad: {
+    type: 'string',
+    since: '3.0.0',
+    source: `_.pad([string=''], [length=0], [chars=' '])`,
+  },
+
+  padEnd: {
+    type: 'string',
+    since: '4.0.0',
+    source: `_.padEnd([string=''], [length=0], [chars=' '])`,
+  },
+
+  padStart: {
+    type: 'string',
+    since: '4.0.0',
+    source: `_.padStart([string=''], [length=0], [chars=' '])`,
+  },
+
+  parseInt: {
+    type: 'string',
+    since: '1.1.0',
+    source: `_.parseInt(string, [radix=10])`,
+  },
+
+  repeat: {
+    type: 'string',
+    since: '3.0.0',
+    source: `_.repeat([string=''], [n=1])`,
+  },
+
+  replace: {
+    type: 'string',
+    since: '4.0.0',
+    source: `_.replace([string=''], pattern, replacement)`,
+  },
+
+  snakeCase: {
+    type: 'string',
+    since: '3.0.0',
+    source: `_.snakeCase([string=''])`,
+  },
+
+  split: {
+    type: 'string',
+    since: '4.0.0',
+    source: `_.split([string=''], separator, [limit])`,
+  },
+
+  startCase: {
+    type: 'string',
+    since: '3.1.0',
+    source: `_.startCase([string=''])`,
+  },
+
+  startsWith: {
+    type: 'string',
+    since: '3.0.0',
+    source: `_.startsWith([string=''], [target], [position=0])`,
+  },
+
+  template: {
+    type: 'string',
+    since: '0.1.0',
+    source: `_.template([string=''], [options={}])`,
+  },
+
+  toLower: {
+    type: 'string',
+    since: '4.0.0',
+    source: `_.toLower([string=''])`,
+  },
+
+  toUpper: {
+    type: 'string',
+    since: '4.0.0',
+    source: `_.toUpper([string=''])`,
+  },
+
+  trim: {
+    type: 'string',
+    since: '3.0.0',
+    source: `_.trim([string=''], [chars=whitespace])`,
+  },
+
+  trimEnd: {
+    type: 'string',
+    since: '4.0.0',
+    source: `_.trimEnd([string=''], [chars=whitespace])`,
+  },
+
+  trimStart: {
+    type: 'string',
+    since: '4.0.0',
+    source: `_.trimStart([string=''], [chars=whitespace])`,
+  },
+
+  truncate: {
+    type: 'string',
+    since: '4.0.0',
+    source: `_.truncate([string=''], [options={}])`,
+  },
+
+  unescape: {
+    type: 'string',
+    since: '0.6.0',
+    source: `_.unescape([string=''])`,
+  },
+
+  upperCase: {
+    type: 'string',
+    since: '4.0.0',
+    source: `_.upperCase([string=''])`,
+  },
+
+  upperFirst: {
+    type: 'string',
+    since: '4.0.0',
+    source: `_.upperFirst([string=''])`,
+  },
+
+  words: {
+    type: 'string',
+    since: '3.0.0',
+    source: `_.words([string=''], [pattern])`,
+  },
+
+}
